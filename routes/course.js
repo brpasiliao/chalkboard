8aa852f966172699a46ac71f525f8fa0808f0677
brpasiliao <brpasiliao@gmail.com>
2021-12-21 16:05:17 -0500

    added instructors tab

--- routes/course.js
+++ routes/course.js
@@ -24,23 +24,32 @@ module.exports = (params) => {
                   section: request.params.section,
                 },
                 (error, c) => {
-                  let template = request.params.tab;
-                  if (request.params.subtab) template = request.params.subtab;
-                  if (request.params.sub2tab) template = request.params.sub2tab;
+                  users
+                    .find({ email: { $in: c.instructors } })
+                    .toArray((error, is) => {
+                      let template = request.params.tab;
+                      if (request.params.subtab)
+                        template = request.params.subtab;
+                      if (request.params.sub2tab)
+                        template = request.params.sub2tab;
 
-                  try {
-                    return response.render("layout", {
-                      pageTitle:
-                        request.params.course + "-" + request.params.section,
-                      nav: "course-nav",
-                      template: template,
-                      user: u,
-                      course: c,
-                      index: request.params.i,
+                      try {
+                        return response.render("layout", {
+                          pageTitle:
+                            request.params.course +
+                            "-" +
+                            request.params.section,
+                          nav: "course-nav",
+                          template: template,
+                          user: u,
+                          course: c,
+                          instructors: is,
+                          index: request.params.i,
+                        });
+                      } catch (err) {
+                        return next(err);
+                      }
                     });
-                  } catch (err) {
-                    return next(err);
-                  }
                 }
               );
             }
